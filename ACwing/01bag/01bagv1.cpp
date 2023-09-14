018f7715ab60acc8b4c2512df66cee088e3879e0
moyihsut <1441339168@qq.com>
2023-09-14 11:39:20 +0800

    01bag

---
+++ ACwing/01bag/01bagv1.cpp
@@ -0,0 +1,44 @@
+/*
+
+dp[j][k]为第j个物品时，在k重量下的最大价值
+递推式：
+1.不选第j个物品，dp[j][k]=dp[j-1][k]
+2.选第j个物品，dp[j][k]=dp[j-1][k-w[j]]+v[j]
+
+初始化：dp[0][0]=0;
+
+*/
+
+#include<algorithm>
+#include<iostream>
+
+using namespace std;
+
+const int MAXN = 1005;
+int v[MAXN];    // 体积
+int w[MAXN];    // 价值 
+int f[MAXN][MAXN];  // f[i][j], j体积下前i个物品的最大价值 
+
+int main() 
+{
+    int n, m;   
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) 
+        cin >> v[i] >> w[i];
+
+    for(int i = 1; i <= n; i++) 
+        for(int j = 1; j <= m; j++)
+        {
+            //  当前背包容量装不进第i个物品，则价值等于前i-1个物品
+            if(j < v[i]) 
+                f[i][j] = f[i - 1][j];
+            // 能装，需进行决策是否选择第i个物品
+            else    
+                f[i][j] = max(f[i - 1][j], f[i - 1][j - v[i]] + w[i]);
+        }           
+
+    cout << f[n][m] << endl;
+
+    return 0;
+}
+
